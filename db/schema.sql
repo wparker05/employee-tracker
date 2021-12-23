97073e98dad48decdc0e54bc40fec3c27f4aeef3
Wesley Rogers <wprogers86@gmail.com>
2021-12-22 21:50:19 -0500

    added seeds.sql and updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE employees (
     last_name VARCHAR(30),
     role_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
+    REFERENCES roles(id) 
     ON DELETE SET NULL,
     manager_id INT REFERENCES employees(id) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO departments (department_name) 
+VALUES ('Sales'),
+       ('Engineering'),
+       ('Finance'),
+       ('Legal');
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Lead',100000,1),
+       ('Salesperson',80000,1),
+       ('Lead Engineer',150000,2),
+       ('Software Engineer',120000,2),
+       ('Account Manager',160000,3),
+       ('Accountant',125000,3),
+       ('Legal Team Lead',250000,4),
+       ('Lawyer',190000,4);
+
+
+INSERT INTO  employees (first_name,last_name,role_id,manager_id)
+VALUES ('Vito','Corleone',1 , 1),
+       ('Sonny','Corleone', 2, NULL),
+       ('Michael','Corleone', 3, 2),
+       ('Vincent','Corleone', 4, NULL),
+       ('Connie','Corleone', 5, 3),
+       ('Johnny','Fontane', 6,NULL),
+       ('Tom','Hagen', 7,4),
+       ('Peter','Clemenza', 8,NULL);
